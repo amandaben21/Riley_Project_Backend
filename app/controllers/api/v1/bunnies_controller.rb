08eafdbcaac50a1a6370db08b7f5800f06543840
Amanda <amandaben21@aol.com>
2021-06-02 15:16:38 -0400

    Bunny controller index & create routes

--- app/controllers/api/v1/bunnies_controller.rb
+++ app/controllers/api/v1/bunnies_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::BunniesController < ApplicationController
+    
+    def index
+        bunnnies = Bunny.all
+        rendor json: bunnies
+    end
+
+    def create
+        bunny = Bunny.new(bunny_params)
+        if bunny.save
+            render json: bunny, status: :accepted
+        else
+            render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+    def bunny_params
+        params require(:bunny).permit(:name, :bio, :image_url, :person_id)
+    end
 end
