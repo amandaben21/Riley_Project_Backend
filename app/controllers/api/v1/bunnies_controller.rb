0fb9bad6d0689b44dce7d6ceac27efe81d025594
Amanda <amandaben21@aol.com>
2021-06-08 02:16:56 -0400

    Created a deleted method

--- app/controllers/api/v1/bunnies_controller.rb
+++ app/controllers/api/v1/bunnies_controller.rb
@@ -9,12 +9,25 @@ class Api::V1::BunniesController < ApplicationController
         bunny = Bunny.new(bunny_params)
         
         if bunny.save
-            render json: BunnySerializer.new(bunny), status: :accepted
+            render json: BunnySerializer.new(bunny)
         else
-            render json: {errors: bunny.errors.full_messages}, status: :unprocessible_entity
+            render json: {error: "Bunny was not saved"}
         end
     end
 
+    def show
+        bunny = Bunny.find_by_id(params[:id])
+        render json: BunnySerializer.new(bunny)
+    end
+
+    def destroy
+        bunny = Bunny.find_by_id(params[:id])
+        bunny.destroy
+        render json: {message: "#{bunny.name} was successfully removed"}
+    end
+    
+    
+    
     private
     def bunny_params
         params.require(:bunny).permit(:name, :age, :gender, :color, :weight, :bio, :image_url, :breed_id)
